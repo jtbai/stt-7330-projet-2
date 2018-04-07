48391b1ecfa3712fa9ee82cc743ea139866a8472
Jean-Thomas Baillargeon <jtbaillargeon@hotmail.com>
2018-04-07 18:30:55 -0400

    random forest on kfold and grid

--- modeling/test-models.R
+++ modeling/test-models.R
@@ -15,6 +15,7 @@ library(MASS)
 library(nnet)
 library(tidyverse)
 library(caret)
+library(randomForest)
 
 # Import data -------------------------------------------------------------
 
@@ -96,7 +97,35 @@ out <- predict(model_qda, data_test)$class
 sum(out == data_test[, surface]) / nrow(data_test)
 
 # Random Forrest
-model_rf <- randomForest(surface ~ ., data = data_train)
+hyper_parameter_grid <- cross_df(
+  list(mtry = seq(4,16,4), ntree = c(700, 1000,2000))
+  )
+
+number_of_hyper_parameter_sets = nrow(hyper_parameter_grid)
+results_by_hyper_parameters <- numeric(number_of_hyper_parameter_sets)
+for(hyper_parameter_index in 1:number_of_hyper_parameter_sets){
+  current_hyper_parameters  = hyper_parameter_grid[hyper_parameter_index, ]
+  fold_results <- numeric(number_of_k_folds)
+  for(current_fold in 1:number_of_k_folds){
+    sub_data <-  obtain_sub_train_test(kfoldable_train_data, current_fold)
+    sub_data_train <- sub_data$train
+    sub_data_test = sub_data$test
+    
+    model = randomForest(surface ~ ., data =sub_data_train,   control = current_hyper_parameters)
+    out = predict(model, sub_data_test, type = "class") 
+    
+    current_result <- sum(out == sub_data_test$surface) / nrow(sub_data_test)
+    fold_results[current_fold] <- current_result
+    print(paste0("Params ",paste(current_hyper_parameters, collapse = "-")," - ","fold ",current_fold, ": accuracy = ", current_result))
+  }
+  results_by_hyper_parameters[hyper_parameter_index] = mean(fold_results)
+}
+
+
+best_hyper_parameters = hyper_parameter_grid[min(which(results_by_hyper_parameters == max(results_by_hyper_parameters))), ]
+
+
+model_rf <- randomForest(surface ~ ., data =sub_data_train,   control = best_hyper_parameters)
 out <- predict(model_rf, data_test)
 sum(out == data_test[, surface]) / nrow(data_test)
 varImpPlot(model_rf)
@@ -105,6 +134,10 @@ varImpPlot(model_rf)
 model_multi <- multinom(surface ~ ., data = data_train)
 out <- predict(model_multi, data_test)
 sum(out == data_test[, surface]) / nrow(data_test)
+
+# SVM
+
+
 
 # Hasard
 props <- tabulate(as.numeric(data_test$surface), nbins = 3) / nrow(data_test)
