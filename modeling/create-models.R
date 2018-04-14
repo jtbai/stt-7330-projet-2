462304e2302c8354ca990e1dfcdc19e99ad0f3d6
Stephane Caron <ste.caron@icloud.com>
2018-04-14 16:10:34 -0400

    Improve modeling pipeline

--- modeling/create-models.R
+++ modeling/create-models.R
@@ -14,29 +14,44 @@ library(nnet)
 library(data.table)
 library(rpart)
 library(jsonlite)
+library(stringr)
 
 # Source functions --------------------------------------------------------
 
 source("modeling/model-factory.R")
 source("modeling/hyper-parameter-selection.R")
+source("modeling/import-data-modeling.R")
+source("modeling/grid-search.R")
 
 
 # Define global configuration ---------------------------------------------
 
 # Do grid search or not
-ind_do_grid_search <- TRUE
+ind_train_model <- FALSE
 
-# 
 train_groups <- 1L
 test_groups <- 2L
 
 
 # Create models -----------------------------------------------------------
 
+data_train <- import_data_modeling("data/data_modeling_classical_methods.csv", ind_train = train_groups, ind_test = test_groups)$data_train
+data_test <- import_data_modeling("data/data_modeling_classical_methods.csv", ind_train = train_groups, ind_test = test_groups)$data_test
 
-
-if (ind_do_grid_search) {
+if (ind_train_model) {
+  train_model(data_train, data_test, path = "modeling/models/")
+} else {
+  actual_models <- list.files("modeling/models/")
+  modeles_to_verify <- c("model_bayes", "model_lda", "model_qda", "model_tree", "model_rf", "model_svm_gaussien", "model_svm_poly3", "model_multi")
+  test <- sapply(modeles_to_verify, function(model) {
+    any(str_detect(actual_models, model))
+  })
   
+  if (any(test == 0)) {
+    stop(paste(c("Some required models are missing from your model folder. Those missing trained models are: ", paste(names(which(test == 0)), collapse = ", "))))
+  }
 }
+
+
 
 
--- modeling/train-models.R
+++ modeling/train-models.R
@@ -1,74 +1,58 @@
 # -------------------------------------------------------------------------
 # Title: train-models
-# Goal: This script takes optimal parameters from grid search as input and train models
+# Goal: Train models by finding optimal parameters through grid search and save models
 # Date: April 2018
 # Author: Stéphane Caron
 # -------------------------------------------------------------------------
 
 
-# Load packages -----------------------------------------------------------
-
-library(e1071)
-library(MASS)
-library(nnet)
-library(data.table)
-library(rpart)
-
-
-# Import parameters -------------------------------------------------------
-
-
-
-
+do_grid_search <- function(data_train, data_test, path_models){
+  
 # Train models ------------------------------------------------------------
+  
+  # Naive bayes
+  model_bayes <-  naiveBayes(surface ~ ., data = data_train)
+  
+  # LDA
+  model_lda <- lda(surface ~ ., data = data_train)
+  
+  # QDA
+  model_qda <- qda(surface ~ ., data = data_train)
+  
+  # Tree-based
+  hyper_parameter <- list(minsplit = c(2, 5, 10), maxdepth = c(1, 3, 5, 8))
+  best_hyper_parameters_tree <-  get_best_hyper_parameters(data_train, "tree_based", 10, hyper_parameter)
+  model_function = get_model_function("tree_based")$model_function
+  model_tree <- model_function(surface ~ ., data = data_train, control = best_hyper_parameters_tree)
+  
+  # Random Forrest
+  hyper_parameter <- list(mtry = seq(4, 16, 4), ntree = c(700, 1000, 2000))
+  best_hyper_parameters_rf <-  get_best_hyper_parameters(data_train, "random_forest", 10, hyper_parameter)
+  model_function = get_model_function("random_forest")$model_function
+  model_rf <- model_function(surface ~ ., data =data_train,   control = best_hyper_parameters_rf)
+  
+  # SVM - gaussien
+  hyper_parameter <- list(epsilon = c(0.1, 0.01, 0.001), cost = c(0.90, 0.95, 0.99, 1))
+  best_hyper_parameters_svm_gaussien <-  get_best_hyper_parameters(data_train, "svm_gaussien", 10, hyper_parameter)
+  model_function = get_model_function("svm_gaussien")$model_function
+  model_svm_gaussien <- model_function(surface ~ ., data =data_train, control= best_hyper_parameters_svm_gaussien)
+  
+  # SVM - polynomial
+  hyper_parameter <- list(epsilon = c(0.1, 0.01, 0.001), cost = c(0.90, 0.95, 0.99, 1))
+  best_hyper_parameters_svm_poly3 <-  get_best_hyper_parameters(data_train, "svm_poly3", 10, hyper_parameter)
+  model_function = get_model_function("svm_poly3")$model_function
+  model_svm_poly3 <- model_function(surface ~ ., data =data_train, control= best_hyper_parameters_svm_poly3)
+  
+  # Modele multinomiale
+  model_multi <- multinom(surface ~ ., data = data_train)
+  
+  list_trained_models <- list(model_bayes, model_lda, model_qda, model_tree, model_rf, model_svm_gaussien, model_svm_poly3, model_multi)
+  names(list_trained_models) <- c("model_bayes", "model_lda", "model_qda", "model_tree", "model_rf", "model_svm_gaussien", "model_svm_poly3", "model_multi")
 
+# Export models -----------------------------------------------------------
 
-
-source("modeling/model-factory.R")
-source("modeling/hyper-parameter-selection.R")
-
-# Import data -------------------------------------------------------------
-
-data_prepared <- fread("data/data_modeling_classical_methods.csv")
-data_prepared[, surface := as.factor(surface)]
-
-
-# Split train/test --------------------------------------------------------
-
-train_groups <- 1L
-test_groups <- 2L
-
-data_train <- data_prepared[split_group %in% train_groups, -(c("split_group")), with = FALSE]
-data_test <- data_prepared[split_group %in% test_groups, -(c("split_group")), with = FALSE]
-
-
-# Search optimal models ---------------------------------------------------
-
-
-# Naive bayes
-model_bayes <-  naiveBayes(surface ~ ., data = data_train)
-
-# Tree-based
-model_function = get_model_function("tree_based")$model_function
-model_tree <- model_function(surface ~ ., data = data_train, control = best_hyper_parameters_tree)
-
-# LDA
-model_lda <- lda(surface ~ ., data = data_train)
-
-# QDA
-model_qda <- qda(surface ~ ., data = data_train)
-
-# Random Forrest
-model_function = get_model_function("random_forest")$model_function
-model_rf <- model_function(surface ~ ., data =data_train,   control = best_hyper_parameters_rf)
-
-# SVM - gaussien
-model_function = get_model_function("svm_gaussien")$model_function
-model_svm_gaussien <- model_function(surface ~ ., data =data_train, control= best_hyper_parameters_svm_gaussien)
-
-# SVM - polynomial
-model_function = get_model_function("svm_poly3")$model_function
-model_svm_poly3 <- model_function(surface ~ ., data =data_train, control= best_hyper_parameters_svm_poly3)
-
-# Model logistic
-model_multi <- multinom(surface ~ ., data = data_train)
+  for (model in seq_along(list_trained_models)){
+    saveRDS(object = list_trained_models[model], file = paste0(path, names(list_trained_models[model]), ".rds"))
+  }
+  
+}
