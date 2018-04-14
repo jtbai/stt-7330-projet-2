169407a482630b5d416d722469dba30d6b3d35f1
Stephane Caron <ste.caron@icloud.com>
2018-04-14 16:54:11 -0400

    Modifiy export function

--- modeling/output_predict.R
+++ modeling/output_predict.R
@@ -10,23 +10,21 @@ source("modeling/test-models.R")
 
 predict_model <- function ( model_name, validation_data ){
    output_predict = predict(model_name,newdata = validation_data)
-   retun(output_predict)
+   return(output_predict)
 }
 
-classical_models = c(model_bayes,tree_based,model_lda,model_qda,random_forest,svm_gaussien,svm_poly3,model_multi)
+classical_models = list(model_bayes,tree_based,model_lda,model_qda,random_forest,svm_gaussien,svm_poly3,model_multi)
 
-for(i in 1:8){
-  predict_models <- mapply(predict_model,classical_models[i],data_prepared[, -(c("split_group"))])
-}
+list_predictions <- lapply(classical_models, function(model) {
+  predict_model(model_name = model, validation_data = data_prepared[, -(c("split_group"))])
+})
 
 
 
 # save output -------------------------------------------------------------------
-fwrite(predict_models, file = "data\predict_naive_bayes.csv")
-fwrite(classical_models[1], file = "data\predict_tree_based.csv")
-fwrite(classical_models[2], file = "data\predict_lda.csv")
-fwrite(classical_models[3], file = "data\predict_qda.csv")
-fwrite(classical_models[4], file = "data\predict_random_forest.csv")
-fwrite(classical_models[5], file = "data\predict_svm_gaussien.csv")
-fwrite(classical_models[6], file = "data\predict_svm_poly3.csv")
-fwrite(classical_models[6], file = "data\predict_model_multi.csv")
+
+vec_models <- c("model_bayes","model_tree","model_lda","model_qda","model_rf","model_svm_gaussien","model_svm_poly3","model_multi")
+
+for (preds in seq_along(list_predictions)) {
+  fwrite(data.frame(list_predictions[[preds]]), file = paste0("data/predictions/predictions_", vec_models[preds], ".csv"))
+}
