e5c294ed1401149720c44fd1674619c04622ca7f
sofia harrouch <sofia.harrouch.1@ulaval.ca>
2018-04-18 20:48:11 -0400

    creation des predictions pour le rapport

--- modeling/create-models.R
+++ modeling/create-models.R
@@ -31,7 +31,7 @@ source("modeling/train-models.R")
 source("modeling/output_predict.R")
 source("modeling/create-predictions-matrix.R")
 source("modeling/train-model-ensemble.R")
-
+source("modeling/preds_rapport.R")
 
 # Define global configuration ---------------------------------------------
 
@@ -103,4 +103,8 @@ if (length(input_model_ensemble) != 0) {
   true_response <- import_data_modeling("data/data_modeling_classical_methods.csv", selected_group = c(train_groups, test_groups, validation_groups))$surface
   train_model_ensemble(predict_matrix, true_response, "accuracy", "data/predictions/", index_train, index_test)
 }
+
+test <- list(index_train = index_train, index_test = index_test, index_validation, index_validation)
+
+predict_models_rapport(models = model_inputs, list_index = list(index_train = index_train, index_test = index_test, index_validation, index_validation), path_rapport ="" , path_preds="data/predictions/")
 
