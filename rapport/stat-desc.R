97839145ac2b764c2fd96dda3ec2fac1882a5ef1
Christopher Blier-Wong <christopher.blierwong@gmail.com>
2018-04-20 14:28:01 -0400

    add description stats to method

--- rapport/stat-desc.R
+++ rapport/stat-desc.R
@@ -1,10 +1,17 @@
+# -------------------------------------------------------------------------
+# Title: stat-desc
+# Goal: This script aims to output simple statistics from dataset
+# Date: April 2018
+# Author: Christopher Blier-Wong
+# -------------------------------------------------------------------------
+
 library(data.table)
 
 data_all <- fread("data/data_modeling_classical_methods.csv")
 
 data_all$split_group <- NULL
 
-boxplot(ace_by_svpt ~ surface, data = data_all)
+boxplot(ace_by_svpt ~ surface, data = data_all, main = "Aces par services", names = c("Hard", "Clay", "Grass"))
 boxplot(first_won_by_first_in ~ surface, data = data_all)
 boxplot(first_won_by_serve_won ~ surface, data = data_all)
 
@@ -26,4 +33,9 @@ table(data_all$surface)
 # variables individuelles -------------------------------------------------
 
 hist(data_all$ace_by_svpt)
-hist(data_all$min_by_svpt)
+
+par(mfrow = c(1, 3))
+hist(data_all[surface == 1]$ace_by_svpt, main = "Hard", xlim = c(0, 0.4), xlab = "ace_by_svpt")
+hist(data_all[surface == 2]$ace_by_svpt, main = "Clay", xlim = c(0, 0.4), xlab = "ace_by_svpt")
+hist(data_all[surface == 3]$ace_by_svpt, main = "Grass", xlim = c(0, 0.4), xlab = "ace_by_svpt")
+
