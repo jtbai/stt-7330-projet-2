ddfd017160811b565c91a13ef76bae6759710ea8
Stephane Caron <ste.caron@icloud.com>
2018-04-20 09:29:16 -0400

    Fix the import function

--- modeling/import-data-modeling.R
+++ modeling/import-data-modeling.R
@@ -7,10 +7,10 @@
 
 import_data_modeling <- function(path, selected_group) {
   
-  data_modeling_classical_methods <- fread(path)
-  data_modeling_classical_methods[, surface := as.factor(surface)]
+  data_modeling <- fread(path)
+  data_modeling[, surface := as.factor(surface)]
   
-  data <- data_modeling_classical_methods[split_group %in% selected_group, -(c("split_group")), with = FALSE]
+  data <- data_modeling[split_group %in% selected_group, -(c("split_group")), with = FALSE]
   
   return(data)
   
